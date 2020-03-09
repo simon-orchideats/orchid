fed05c844d90c7beef48b0883dfc2455cd17f57a
Simon Vuong <simon.vuong.saute@gmail.com>
2020-03-09 10:56:59 -0400

    making notes

--- src/server/orders/orderService.ts
+++ src/server/orders/orderService.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import { EOrder, IOrder } from './../../order/orderModel';
 import { IMeal } from './../../rest/mealModel';
 import { getPlanService } from './../plans/planService';
@@ -337,58 +335,342 @@ class OrderService {
       throw new Error('Internal Server Error');
     }
   }
+
+  private async getOrder(orderId: string, fields?: string[]) {
+    const options: any = {
+      index: ORDER_INDEX,
+      id: orderId,
+    };
+    if (fields) options._source = fields;
+    try {
+      const res: ApiResponse<EOrder> = await this.elastic.getSource(options);
+      const order: any = res.body;
+      order._id = orderId;
+      return order as IOrder;
+    } catch (e) {
+      console.error(`[OrderService] failed to get order '${orderId}'`, e.stack);
+      return null;
+    }
+  }
   
   async updateOrder(signedInUser: SignedInUser, orderId: string, cart: ICartInput): Promise<MutationBoolRes> {
-    const validation = await validateCart(cart);
-    if (validation) {
-      return {
-        res: false,
-        error: validation
-      };
-    }
-    if (!signedInUser) {
-      const msg = `No signed-in user`;
-      console.warn('[OrderService]', msg)
-      return {
-        res: false,
-        error: msg
+    try {
+      const validation = await validateCart(cart);
+      if (validation) {
+        return {
+          res: false,
+          error: validation
+        };
       }
-    }
-    if (!orderId) {
-      const msg = `No order id user`;
-      console.warn('[OrderService]', msg)
-      return {
-        res: false,
-        error: msg
+      if (!signedInUser) {
+        const msg = `No signed-in user`;
+        console.warn('[OrderService]', msg)
+        return {
+          res: false,
+          error: msg
+        }
       }
-    }
-    const stripeCustomerId = signedInUser.stripeCustomerId;
-    const subscriptionId = signedInUser.stripeSubscriptionId
-    if (!stripeCustomerId) {
-      const msg = 'Missing stripe customer id';
-      console.warn('[OrderService]', msg)
-      return {
-        res: false,
-        error: msg
+      if (!orderId) {
+        const msg = `No order id user`;
+        console.warn('[OrderService]', msg)
+        return {
+          res: false,
+          error: msg
+        }
       }
-    }
-    if (!subscriptionId) {
-      const msg = 'Missing subscription id';
-      console.warn('[OrderService]', msg)
-      return {
-        res: false,
-        error: msg
+      const stripeCustomerId = signedInUser.stripeCustomerId;
+      const subscriptionId = signedInUser.stripeSubscriptionId
+      if (!stripeCustomerId) {
+        const msg = 'Missing stripe customer id';
+        console.warn('[OrderService]', msg)
+        return {
+          res: false,
+          error: msg
+        }
       }
-    }
+      if (!subscriptionId) {
+        const msg = 'Missing subscription id';
+        console.warn('[OrderService]', msg)
+        return {
+          res: false,
+          error: msg
+        }
+      }
+  
+      /**
+       * 
+       * final plan.never discount the modified week. even if the day is changed, we keep the same billing date.
+       * once hte invoice.created event is published we you can no longer skip it. but you can still change the date
+       * and change the meals. when changing date, dDate must be SAME OR BEFORE 2 days after the next billing date. (this
+       * is to prevent infinite push-backs)
+       * 
+       * ex:
+       * 
+       * today is 2/26 and bDate is every thursday, so 2/27 followed by 3/5. so the farthest i can push back 2/27's order is
+       * 3/7 and the last day i can change is 3/5
+       * 
+       * so assuming you are not skipping, any change you make updates the bill for 3/5 by either increasing or decreasing it.
+       * increase when i change to a higher plan, and decrease when i change to a lower plan
+       * 
+       * 
+       * repeat the same for nextnext. this works because for a given week, i awlays pay and i "catch up" on the next week.
+       * if they cancel before the next week, i just need to make sure i force a remaining payment.
+       */
+  
+      const oldOrder = await this.getOrder(orderId);
+      if (!oldOrder) throw new Error(`Couldn't get order '${orderId}'`);
 
-    await this.stripe.invoiceItems.create({
-      subscription: subscriptionId,
-      customer: 'cus_GnNoqL1OSGOfga',
-      amount: 2500,
-      description: 'sup',
-    });
-
-
+      const subscription = await this.stripe.subscriptions.retrieve(subscriptionId);
+      /**
+       * use subscription.billing_cycle_anchor to determine if we're updating first or second upcomingOrder
+       * 
+       * FINAL PLAN 1 - USE SCHEDULES ONLY
+       * 
+       * first
+       *  hasFirst already been billed?
+       *    yes or no, doesn't matter. we treat this the same
+  *         await this.stripe.subscriptionSchedules.create({
+              customer: stripeCustomerId,
+              start_date: 'first billing cycle + 1 d',
+              phases: [
+                { //PHASE 1 (week2)
+                  plans: [
+                    {
+                      plan: '8 meal plan',
+                      quantity: 1
+                      coupon: isDowngrade ? week1paid - week1cost // if we week1 downgraded
+                    } // original plan.
+                    // if plan was unchanged then leave as is
+                    {plan: '12 meal plan', quantity: 1} // if we upgraded (choose the proper downgrade, obv) $$$$$$$$$$$$$$$$$$$ NOOOO. this is NOT RIGHT. CUSTOMER OVERPAID!!!! $$$$$$$$$$$$ (unless i make a in-between plan which is a terrible idea cuz no way im gonna maintain that)
+                  ],
+                  iterations: 1,
+                },
+                { //PHASE 2 (week3)
+                  plans: [
+                    {plan: 'Gold special', quantity: 1}, // original plan to reset
+                  ],
+                  iterations: 1,
+                },
+              ],
+            });
+       * 
+       *  
+       * 
+       * second
+       *    did i update first?
+       *      yes
+       *        this means there's a schedule already
+       *        find the current phase based on start/end of scheudle.start/end vs schedule.phases.start/end
+       *        i'm in first phase
+       *            - yes
+       *                this means i haven't paid for week2 yet. so but either way, i still need to update phase2 as
+       *                i need to migrate the changes to the cost of phase2 which represents week3
+       *                
+       * 
+       *        im in second phase
+       *            yes
+       *                this means the first week has been taken care of and is done. AND i already paid for week2.
+       *                so i need to move these changes into week3 by updating PHASE2 which is the phase for week3.
+       * 
+       *      no
+       *        create a schedule
+      *         await this.stripe.subscriptionSchedules.create({
+                  customer: stripeCustomerId,
+                  start_date: 'week2 billing cycle + 1 d',
+                  phases: [
+                    { //PHASE 1 //week3
+                      plans: [
+                        {plan: 'Gold special', quantity: 1} // original plan
+                        {plan: '4 meal plan', quantity: 1} // if we upgraded (choose the proper downgrade, obv)
+                        {plan: '12 meal plan', quantity: 1} // if we downgraded (choose the proper upgrade, obv)
+                      ],
+                      iterations: 1,
+                    },
+                    { //PHASE 2 //week4
+                      plans: [
+                        {plan: 'Gold special', quantity: 1}, // original plan to reset
+                      ],
+                      iterations: 1,
+                    },
+                  ],
+                });
+       * 
+       * 
+       *    
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * ANOTHER FINAL????? the plan is to make weekN handle it, otherwise make weekN +1 handle it
+       * 
+       * references
+       *  https://stripe.com/docs/billing/subscriptions/discounts#other (negative invoices are allowed)
+       * 
+       * week1 update
+       *    did customer pay for week1 yet?
+       *      yes
+       *        if upgrade - add invoice for (upgradePrice - week1Total) (affects week2)
+       *        if downgrade - adds  invoice for (downgradePrice - week1Total ) (affects week2)
+       *        if maintain - do nothing
+       *      no
+       *        if upgrade - add invoice for (upgradePrice - week1Total) (affects week1)
+       *        if downgrade - add invoice for (downgradePrice - week1Total ) (affects week1)
+       *        if maintain - do nothing
+       * 
+       * week2 update
+       *    did customer update week1 after week1's payment? we only care about after week1's payment updates because
+       *    otherwise week1's payment handled it already.
+       *      yes
+       *        this means there's an existing extra invoice. but this doesn't matter cuz we just add another invoice item
+      *         did customer pay for week2 yet?
+    *             yes
+        *           if upgrade - add invoice for (upgradePrice - week1Total) (affects week2)
+        *           if downgrade - adds invoice for (downgradePrice - week1Total ) (affects week2)
+        *           if maintain - do nothing
+        *           if skip - credit back all
+      *           no
+        *           if upgrade - add invoice for (upgradePrice - week1Total) (affects week3)
+        *           if downgrade - adds invoice for (downgradePrice - week1Total ) (affects week3)
+        *           if maintain - do nothing
+       *      no-no problemo. just treat the same
+    *         did customer pay for week2 yet?
+  *             yes
+      *           if upgrade - add invoice for (upgradePrice - week1Total) (affects week2)
+      *           if downgrade - adds invoice for (downgradePrice - week1Total ) (affects week2)
+      *           if maintain - do nothing
+    *           no
+      *           if upgrade - add invoice for (upgradePrice - week1Total) (affects week3)
+      *           if downgrade - adds invoice for (downgradePrice - week1Total ) (affects week3)
+      *           if maintain - do nothing
+       * 
+       * 
+       *
+       * - weekN update
+       *    - did consumer pay for weekN yet?
+       *      - yes
+       *        - invoice for weekN+1.
+       *        - is there already a "modification" invoice for weekN+1?
+       *            -yes
+       *              remove it
+       *        // it's possible theres weekN contains weekN-1 invoices and we ignore them from the math.
+       *        // those are a done deal
+       *        - upgrade = create invoice for (newPlanPrice - (weekNPlan + weekNINvoices))
+       *        - downgrade = create invoice for (newPlanPrice - (weekNPlan + weekNINvoices))
+       *        - maintain
+       *        - skip = create invoice for (0 - (weekNPlan + weekNINvoices))
+  *             - add if there's an invoice, add it to weekN+1
+       *        
+       *      no
+       *        - invoice for weekN
+       *        - is there already a "modification" invoice for weekN?
+       *            - yes
+       *              - remove it
+       *        - upgrade = create invoice for (newPlanPrice - original weekNTotal)
+       *        - downgrade = create invoice for (newPlanPrice - original weekNTotal)
+       *        - maintain
+       *        - skip = create invoice for (0 - original weekNTotal)
+       *        - if we just created a invoice, add it to weekN
+       * 
+       * 
+       *  get upcoming invoice
+       *    loop through line items, 
+       *      is there item where description contains "Plan adjustment" 
+        *      yes
+        *        delete invoice item stripe.invoiceItems.del
+       *      prevPlanPrice = get plan from loop  
+       * 
+       *    is now within the currInvoice period?
+       *      yes
+       *        basePrice = prevPlanPrice
+       *      no
+       *        basePrice = prevPlanPrice - 
+       *        
+       * 
+       *    // new plan price is 0 when skipping
+       *    create invoie for (newPlanPrice - prevPlanPrice
+       * 
+       * 
+       * 
+       *      
+       *    
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       *  
+       * 
+       * 
+       * 
+       * given week, modify week
+       * 
+       * w1, w1 - upgrade w1 40 -> 80, so gets INVOICED w1 (80 - 40) = 40
+       * 
+       * w1, w1 - upgrade w1 again to 150, remove w1 prevInvoice (40) and reinvoice w1 (150 - 40) = 90
+       * 
+       * w1, w1 - downgrade w1 all the way to 40, remove w1 prevInvoice (90) and reinvoice w1 (40 - 40) = 0;
+       * 
+       * w1, w1 - upgrade w1 to 80, remove w1 prevInvoice(0) and reinvoice w1 (80 - 40) = 40
+       * 
+       * ---------W1 STILL NOT SHIPPED, BUT CUST PAID 80 TOTAL (40(plan) + 40(inv)) ----------
+       * 
+       * w2, w1 - upgrade w1 to 150, invoice w2 (150 - 80) = 70
+       * 
+       * w2, w1 - downgrade w1 to 80, invoice w2 (80 - 80) = 0 (THIS IS RIGHT SINCE THEY ALREADY PAID 80)
+       * 
+       * w2, w1 - downgrade w1to 40, invoice w2 (40 - 80) = -40
+       * 
+       * w2, w1 - skip, w1 invoice w2 (0 - 80) = -80
+       * 
+       * 
+       * --------------- W1 done. final is skip so invoiced -80 to cover overpayment of 80 in w1------------
+       * 
+       * w2, w2 - upgrade w2 40 -> 150, so gets invoiced w2 150 - 40 = 110
+       * 
+       * w2, w2 - downgrade w2 150 -> 80, so remove w2 prevInvoice (110) and reinvoice reinvoice w2 (80 - 40) = 40
+       * 
+       * -----------W2 STILL NOT SHIPPED BUT CUST PAID -80 (from w1) + 40 (from w2) = -40 ------------
+       * 
+       * // this is correct because customer is invoiced the -40 to counter the 40 in w2.
+       * w3, w2 - skip w2, invoice w3 (0 - 40 [40 because w2 payment for w2 stuff [ignoring w1 invoice within w2] is 40]) = -40
+       * 
+       * // this correct cuz customer's w2 progression is 40 -> 150 -> 80 -> 0 -> 40.
+       * w3, w2 - upgrade w2 40, remove w3 prevInvoice (-40) and invoice w3 (40 - 40+40 [40 + 40 because original w2 + w2's specific modificaiton invoices]) = 40
+       *                                                          
+       * w3, w3 - upgrade w3 to 80, NO W3 TARGET invoice (only w2), invoice w3 (80 - 40) = 40
+       * 
+       * --------------W2 SHIPS AND W3 PAID. FINAL W2 = 40, W3 = 80 (40 + 40)
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       * 
+       */
+    } catch (e) {
+      console.error(`[OrderService] couldn't updateOrder for '${orderId}' with cart '${cart}'`, e.stack);
+      throw new Error('Internal Server Error');
+    }
   }
 }
 
