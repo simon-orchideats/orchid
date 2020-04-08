f0ad3ee8ac523a8d57f91d565b61f17690f177d6
keri4141 <alvinchau2013@gmail.com>
2020-04-08 12:55:09 -0400

    302 means temporary redirect

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -3,11 +3,12 @@ import { accessTokenCookie, refreshTokenCookie } from '../../utils/auth';
 import { activeConfig } from '../../config';
 
 export default (_req: NextApiRequest, res: NextApiResponse ) => {
-  res.writeHead( 301,{
-    'Location': `${activeConfig.client.auth.logOut}`, 
+  res.writeHead( 302,{
     'Set-Cookie': [
       `${accessTokenCookie}=''; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; path=/`,
       `${refreshTokenCookie}=''; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; path=/`],
+    'Location': `${activeConfig.client.auth.logOut}`, 
+    
   })
   res.end();
 }
