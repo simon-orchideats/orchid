98bda8ba6d4261a1b52112f494170a621ff08f2b
Simon Vuong <simon.vuong.saute@gmail.com>
2020-05-01 18:30:34 -0400

    merge with stage

--- src/config.ts
+++ src/config.ts
@@ -335,8 +335,8 @@ const getConfig = (env?: string) => {
     console.log('running stage');
     return stage;
   } else if (env === 'production') {
-    console.log('running stage');
-    return stage;
+    console.log('running production');
+    return production;
   }
   console.log('running default');
   return development;
