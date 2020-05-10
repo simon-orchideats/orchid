7e60c61b22ffe76a43baf06b02541eadcc9b4458
Simon Vuong <simon.vuong.saute@gmail.com>
2020-05-10 01:07:44 -0400

    when canceling subscriptions, delete order if no confirmed deliveries

--- src/config.ts
+++ src/config.ts
@@ -335,7 +335,7 @@ const getConfig = (env?: string) => {
     console.log('running stage');
     return stage;
   } else if (env === 'production') {
-    console.log('running stage');
+    console.log('running production');
     return production;
   }
   console.log('running development');
