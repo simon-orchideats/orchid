9d52e69c0b1088c63c6feb78f562ab044ffce172
Simon Vuong <simon.vuong.saute@gmail.com>
2020-06-07 16:27:44 -0400

    turn on amplitude for stage

--- src/config.ts
+++ src/config.ts
@@ -175,8 +175,8 @@ const stage: config = {
     },
     analytics: {
       amplitude: {
-        //key: 'd9a3d86674a064af5f76e222fba9bad6',
-         key: '',
+        key: 'd9a3d86674a064af5f76e222fba9bad6',
+        //  key: '',
       },
       ga: {
         // trackingId: 'UA-158803692-1',
