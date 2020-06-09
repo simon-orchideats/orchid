7cbba33bed0671263febc5e3369db88c0410ae4d
Simon Vuong <simon.vuong.saute@gmail.com>
2020-06-09 04:43:07 -0400

    fix fb tracking pixel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,31 +33,35 @@ export default class MyDocument extends Document {
               `,
             }}
           />
-        {/*Facebook Pixel */}
-        <script 
-          dangerouslySetInnerHTML={{ 
-            __html:`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq(${fbEvents.INIT}, ${fbTracking});
-              fbq(${fbEvents.TRACK}, ${fbEvents.PAGE_VIEW});
-            ` 
-          }}/>
-
+          {/*Facebook Pixel */}
+          <script 
+            dangerouslySetInnerHTML={{ 
+              __html:`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('${fbEvents.INIT}', '${fbTracking}');
+                fbq('${fbEvents.TRACK}', '${fbEvents.PAGE_VIEW}');
+              ` 
+            }}
+          />
           <noscript 
             dangerouslySetInnerHTML={{ 
               __html:`
-                <img height="1" width="1" style="display:none"
-                src="https://www.facebook.com/tr?id=${fbTracking}&ev=PageView&noscript=1"
+                <img
+                  height="1"
+                  width="1"
+                  style="display:none"
+                  src="https://www.facebook.com/tr?id=${fbTracking}&ev=PageView&noscript=1"
                 />
               ` 
-          }}/>
+            }}
+          />
         
           <meta property="og:image" content="/logo.png" />
           {/* PWA primary color */}
