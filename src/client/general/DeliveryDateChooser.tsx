ba66207f0df119b78a9e4779c6e4e4c0806fe87d
Simon Vuong <simon.vuong.saute@gmail.com>
2020-06-22 16:08:28 -0400

    bugfix. when updating an order and tuesday delivery is disabaled due to being too far in advance, we properly default to the next avaialble day instead of always defaulting to tuesday

--- src/client/general/DeliveryDateChooser.tsx
+++ src/client/general/DeliveryDateChooser.tsx
@@ -5,6 +5,7 @@ import { deliveryDay, deliveryTime, ConsumerPlan } from "../../consumer/consumer
 import withClientApollo from '../utils/withClientApollo';
 import { getNextDeliveryDate } from '../../order/utils';
 import { useGetConsumer } from '../../consumer/consumerService';
+import { useMemo, useEffect } from 'react';
 
 const useStyles = makeStyles(theme => ({
   smallPaddingBottom: {
@@ -34,6 +35,24 @@ const DeliveryDateChooser: React.FC<{
 }) => {
   const classes = useStyles();
   const consumer = useGetConsumer();
+
+  const disables = useMemo(() => Array.from(
+    [ 0, 1, 2, 3, 4, 5, 6, ],
+    (d: deliveryDay) => !!limit && getNextDeliveryDate(d).valueOf() >= limit
+  ), [ limit ]);
+
+  useEffect(() => {
+    if (disables[day]) {
+      const allowedDay = disables.findIndex(d => d === false);
+      if (allowedDay === -1) {
+        const err = new Error(`Failed to find allowed day with limit '${limit}'`)
+        console.error(err.stack);
+        throw err;
+      }
+      onDayChange(allowedDay as deliveryDay)
+    }
+  });
+
   return (
     <>
       <ToggleButtonGroup
@@ -46,34 +65,31 @@ const DeliveryDateChooser: React.FC<{
           onDayChange(d);
         }}
       >
-        <ToggleButton value={0} disabled={!!limit && getNextDeliveryDate(0).valueOf() >= limit}>
+        <ToggleButton value={0} disabled={disables[0]}>
           Su
         </ToggleButton>
-        <ToggleButton value={1} disabled={!!limit && getNextDeliveryDate(1).valueOf() >= limit}>
+        <ToggleButton value={1} disabled={disables[1]}>
           M
         </ToggleButton>
-        <ToggleButton value={2} disabled={!!limit && getNextDeliveryDate(2).valueOf() >= limit}>
+        <ToggleButton value={2} disabled={disables[2]}>
           T
         </ToggleButton>
-        <ToggleButton value={3} disabled={!!limit && getNextDeliveryDate(3).valueOf() >= limit}>
+        <ToggleButton value={3} disabled={disables[3]}>
           W
         </ToggleButton>
-        <ToggleButton value={4} disabled={!!limit && getNextDeliveryDate(4).valueOf() >= limit}>
+        <ToggleButton value={4} disabled={disables[4]}>
           Th
         </ToggleButton>
-        <ToggleButton value={5} disabled={!!limit && getNextDeliveryDate(5).valueOf() >= limit}>
+        <ToggleButton value={5} disabled={disables[5]}>
           F
         </ToggleButton>
-        <ToggleButton value={6} disabled={!!limit && getNextDeliveryDate(6).valueOf() >= limit}>
+        <ToggleButton value={6} disabled={disables[6]}>
           Sa
         </ToggleButton>
       </ToggleButtonGroup>
-      {
-        (!consumer.data || !consumer.data.Plan) &&
         <Typography variant='body1'>
-          <b>First delivery day: {getNextDeliveryDate(day).format('M/D/YY')}</b>
+          <b>{(consumer.data && consumer.data.Plan) ? 'Delivery day' : 'First delivery day'}: {getNextDeliveryDate(day).format('M/D/YY')}</b>
         </Typography>
-      }
       <FormControl variant='filled' className={`${classes.input} ${classes.smallPaddingBottom}`}>
         <Select
           value={time}
