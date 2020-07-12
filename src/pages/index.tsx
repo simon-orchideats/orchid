dd7c59ef67000ec4e18ffe6b0fbfee94b148b95b
Simon Vuong <simon.vuong.saute@gmail.com>
2020-07-12 18:52:44 -0400

    remove icon links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -765,11 +765,6 @@ const Index = () => {
       <HowItWorks />
       <Testimonials />
       <Footer />
-      <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-      Icons made by <a href="https://www.flaticon.com/authors/flat-icons" title="Flat Icons">Flat Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-      <div>Icons made by <a href="https://www.flaticon.com/free-icon/meal_1919230" title="Nikita Golubev">Nikita Golubev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-      <div>Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-      <div>Icons made by <a href="https://www.flaticon.com/authors/bqlqn" title="bqlqn">bqlqn</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </>
   )
 }
