049eed207bcb8d630cb94b94cb10e8aa63ac0ddc
Simon Vuong <simon.vuong.saute@gmail.com>
2020-07-24 13:23:50 -0400

    add plans to zip modal

--- src/client/plan/PlanDetails.tsx
+++ src/client/plan/PlanDetails.tsx
@@ -3,18 +3,18 @@ import { makeStyles } from '@material-ui/core';
 import { Tier } from '../../plan/planModel';
 
 const useStyles = makeStyles(theme => ({
-  card: {
+  card: ({ color, small }: { color: string, small: boolean }) => ({
     textAlign: 'center',
-    marginLeft: theme.spacing(3),
-    marginRight: theme.spacing(3),
-    marginTop: theme.spacing(1),
+    marginLeft: theme.spacing(small ? 0 : 3),
+    marginRight: theme.spacing(small ? 1 : 3),
+    marginTop: theme.spacing(small ? 0 : 1),
     marginBottom: theme.spacing(1),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    width: 260,
+    paddingLeft: theme.spacing(small ? 0 : 2),
+    paddingRight: theme.spacing(small ? 0 : 2),
+    width: small ? 200 : 260,
     borderStyle: 'solid',
-    borderColor: '#ed8d81',
-  },
+    borderColor: color === 'black' ? theme.palette.primary.main : '#ed8d81',
+  }),
   cardSubtitle: {
     color: theme.palette.text.secondary,
   },
@@ -22,8 +22,14 @@ const useStyles = makeStyles(theme => ({
 
 const PlanDetails: React.FC<{
   tier: Tier;
-}> = ({ tier }) => {
-  const classes = useStyles();
+  color?: string,
+  small?: boolean,
+}> = ({
+  tier,
+  color = 'default',
+  small = false
+}) => {
+  const classes = useStyles({ color, small });
   const meals = (
     <Typography variant='h6'>
       {tier.minMeals}+ meals
